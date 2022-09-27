fc82293995484c7db6879e44c8dff4f4eeb4233f
SebastianJimenez2510 <sebastian.jimenezgarro@gmail.com>
2022-09-27 02:50:11 +0000

    Carrusel

---
+++ src/js/component/carrusel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Carrusel = () => {
+
+    return (<>
+    <div className="container bg-secondary">
+        <h1 className="text-start"> A Warm Welcome!</h1>
+        <p className="text-start">These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided.</p>
+        <div className="text-start">
+        <button type="button" class="btn btn-primary">Call to action!</button>
+        </div>
+    </div>
+    </> )
+}
